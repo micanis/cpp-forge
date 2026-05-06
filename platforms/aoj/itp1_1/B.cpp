ee975f5ce105b6f34ad24e9e33e866ea635aba9e
micanis <micanis>
2026-05-07 08:33:28 +0900

    solve:itp1_1/B.cpp

--- platforms/aoj/itp1_1/B.cpp
+++ platforms/aoj/itp1_1/B.cpp
@@ -22,11 +22,18 @@ using ll = long long;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 
-template <class T> bool chmin(T &a, const T &b) { return a > b ? a = b, true : false; }
-template <class T> bool chmax(T &a, const T &b) { return a < b ? a = b, true : false; }
+template <class T>
+bool chmin(T &a, const T &b) {
+  return a > b ? a = b, true : false;
+}
+template <class T>
+bool chmax(T &a, const T &b) {
+  return a < b ? a = b, true : false;
+}
 
 #ifdef DEBUG
-template <class A, class B> ostream &operator<<(ostream &os, const pair<A, B> &p) {
+template <class A, class B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) {
   return os << "(" << p.first << ", " << p.second << ")";
 }
 template <class T, class = decltype(begin(declval<T>())),
@@ -50,7 +57,9 @@ int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-
+  int n;
+  cin >> n;
+  cout << n * n * n << '\n';
 
   return 0;
 }
